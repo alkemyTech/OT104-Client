2751fe508521e350463cb40c22fced276e216696
Edgar Britez <edgarfbritez@gmail.com>
2021-11-23 19:57:21 -0300

    welcome message for the screen dashboard

---
+++ src/Components/ScreenDashboard/ScreenDashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./ScreenDashboard.module.css";
+
+function ScreenDashboard() {
+  return (
+    <div className={styles.ScreenDashboardConatiner}>
+      <h2 className={styles.screenDashBoardTitle}>Welcome to Dashboard</h2>
+    </div>
+  );
+}
+
+export default ScreenDashboard;
+
+// questions
+// 1. can I use styled components here?
