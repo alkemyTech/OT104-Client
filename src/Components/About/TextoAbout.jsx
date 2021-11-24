f650863ab7968569ecf6813eea52c879000bd10e
unknown <janoncarlos@gmail.com>
2021-11-24 17:40:37 -0300

    component About title text app route text form apiw/useEffect

---
+++ src/Components/About/TextoAbout.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
+// import { useDispatch } from 'react-redux'
+
+const TextoAbout = () => {
+  // const dispatch = useDispatch()
+  const [aboutText, setAboutText] = useState('');
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
+      setAboutText(response.data[0].body)
+
+    }
+    loadUsers()
+
+  }, []);
+
+  return (
+    <div>
+      <h1>{aboutText}</h1>
+    </div>
+  )
+}
+
+export default TextoAbout
---
+++ src/Components/About/TituloAbout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TituloAbout = () => {
+
+
+
+  return (
+    <div>
+      <h1>NOSOTROS</h1>
+
+    </div>
+  )
+}
+
+export default TituloAbout
+
