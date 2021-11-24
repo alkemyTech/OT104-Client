4a1cab5e6f431361e1e44d7a59387c30d64e5726
Mauricio Romero <mauricionromero@hotmail.com>
2021-11-24 15:01:09 -0300

    implemented Formik with Yup

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -1,83 +1,109 @@
-import React, { useState } from "react";
-import "../../Components/FormStyles.css";
+import React, { useState, useEffect } from "react";
+import { ErrorMessage, Form, Field, FormikProvider, useFormik } from "formik";
+import * as Yup from "yup";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+import "../../Components/FormStyles.css";
 
-const NewsForm = () => {
+const categoriesEndPoint = [
+  { id: "4sd6", name: "Categoria 1" },
+  { id: "1sd7", name: "Categoria 2" },
+  { id: "48e6", name: "Categoria 3" },
+  { id: "6sd3", name: "Categoria 4" },
+];
+
+const NewsForm = ({ newToEdit = false }) => {
   const [imgPreview, setImgPreview] = useState("");
-  const [initialValues, setInitialValues] = useState({
-    title: "",
-    image: "",
-    content: "",
-    category: "",
+  const [imageState, setImageState] = useState("");
+  const [categories, setCategories] = useState([]);
+  const initialValues = {
+    title: newToEdit.title || "",
+    image: newToEdit.image || "",
+    content: newToEdit.content || "",
+    category: newToEdit.category || "",
+  };
+
+  useEffect(() => {
+    setCategories(categoriesEndPoint);
+  }, []);
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().min(4).required("El titulo es requerido"),
+    image: Yup.string().required("La imagen es requerida"),
+    content: Yup.string().required("El contenido es requerido"),
+    category: Yup.string().required("La categoria es requerida"),
   });
 
-  const handleChange = (e) => {
-    if (e.target.name === "title") {
-      setInitialValues({ ...initialValues, title: e.target.value });
-    }
-    if (e.target.name === "image") {
-      setInitialValues({ ...initialValues, image: e.target.files[0] });
-      setImgPreview(URL.createObjectURL(e.target.files[0]));
-    }
-    if (e.target.name === "category") {
-      setInitialValues({ ...initialValues, category: e.target.value });
-    }
-  };
-  const handleEditorChange = (e, editor) => {
-    const data = editor.getData();
-    setInitialValues({ ...initialValues, content: data });
+  useEffect(() => {
+    formik.setFieldValue("image", imageState);
+  }, [imgPreview]);
+
+  const handleFileChange = (e) => {
+    setImageState(e.target.files[0]);
+    setImgPreview(URL.createObjectURL(e.target.files[0]));
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(initialValues);
+  const handleEditorChange = (_, editor) => {
+    const data = editor.getData();
+    formik.setFieldValue("content", data);
   };
+
+  const onSubmit = (values) => {
+    console.log("Values:", values);
+  };
+
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+    validationSchema,
+  });
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-field"
-        type="text"
-        name="title"
-        placeholder="Titulo"
-        value={initialValues.title || ""}
-        onChange={handleChange}
-      ></input>
+    <FormikProvider value={formik}>
+      <Form className="form-container">
+        <Field
+          className="input-field"
+          type="text"
+          name="title"
+          placeholder="Titulo"
+        />
+        <ErrorMessage name="title" component="div" />
 
-      {imgPreview && <img src={imgPreview} alt="preview" />}
-      <input
-        className="input-field"
-        type="file"
-        name="image"
-        accept="image/*"
-        onChange={handleChange}
-      ></input>
+        {imgPreview && <img src={imgPreview} alt="preview" />}
+        <input
+          className="input-field"
+          type="file"
+          name="image"
+          accept="image/*"
+          onChange={handleFileChange}
+        />
+        <ErrorMessage name="image" component="div" />
 
-      <CKEditor
-        name="content"
-        className="input-field"
-        editor={ClassicEditor}
-        onChange={handleEditorChange}
-      />
+        <CKEditor
+          name="content"
+          className="input-field"
+          editor={ClassicEditor}
+          onChange={handleEditorChange}
+        />
+        <ErrorMessage name="content" component="div" />
 
-      <select
-        className="select-field"
-        name="category"
-        value={initialValues.category || ""}
-        onChange={handleChange}
-      >
-        <option value="" disabled>
-          Select category
-        </option>
-        <option value="1">Demo option 1</option>
-        <option value="2">Demo option 2</option>
-        <option value="3">Demo option 3</option>
-      </select>
-      <button className="submit-btn" type="submit">
-        Send
-      </button>
-    </form>
+        <Field component="select" className="select-Fieldield" name="category">
+          <option value="" disabled>
+            Categoría
+          </option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </Field>
+        <ErrorMessage name="category" component="div" />
+
+        <button className="submit-btn" type="submit">
+          Guardar
+        </button>
+      </Form>
+    </FormikProvider>
   );
 };
 
