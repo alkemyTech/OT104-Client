9198a3439d3dba13ecf2210bbc9816481c17eec8
Ignacio Hardoy <hartoyy@hotmail.com>
2021-11-24 16:51:58 -0300

    form validation ok, ticket for code review

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -3,56 +3,115 @@ import '../FormStyles.css';
 import { Formik } from 'formik';
 
 const RegisterForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        lastName: ''
-    })
     
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'lastName'){
-            setInitialValues({...initialValues, lastName: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
 
     return (
-     <Formik>
+     <Formik
+     initialValues={{ 
+        name:"",
+        lastName:"",
+        email:"",
+        password:"",
+        repassword:""
+      }}
+
+      validate={(values)=>{
+        let errores = {};
+  
+        //Validacion nombre
+        if(!values.name){
+          errores.name = "Por favor ingresa un nombre"
+        }
+  
+        //Validacion last nambre
+        if(!values.lastName){
+          errores.lastName = "Por favor ingresa un apellido"
+        }
+        
+        //Validacion email
+        if(!values.email){
+            errores.email = "Por favor ingresa un email"
+          }
+
+        //Validacion password
+        if(!values.password){
+            errores.password = "Por favor ingresa una contraseña"
+            console.log(values.password);
+          }else if  (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(values.password)){
+            errores.password = "La contraseña debe tener una longitud mínima de 6 caraceteres, y contener al menos un número, una letra y un símbolo"
+          }
+          
+
+        //Validacion repassword
+        if(values.repassword !== values.password){
+            errores.repassword = "Las contraseñas no coinciden"
+          }  
+
+
+        return errores;
+      }}
+
+      onSubmit={(values, {resetForm}) =>{
+        var userInfo = {
+          name:values.name,
+          lastName:values.lastName,
+          email:values.email,
+          password:values.password
+      };
+         console.log(userInfo);
+        //resetForm();     
+      }}
+     >
+      {( {values, errors, touched, handleSubmit, handleChange, handleBlur} )=>(   
         <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" 
-             type="text" name="name" 
-             value={initialValues.name} 
-             onChange={handleChange} placeholder="Enter name"></input>
-
-            <input className="input-field" 
-             type="text" name="lastName" 
-             value={initialValues.lastName} 
-             onChange={handleChange} placeholder="Enter last name"></input>
-
             <input className="input-field" 
              type="text" 
              name="name" 
-             value={initialValues.name} 
-             onChange={handleChange} placeholder="Enter email"></input>
+             value={values.name} 
+             placeholder="Enter name"
+             onChange={handleChange}
+             onBlur={handleBlur} required></input>
+             {touched.name && errors.name && <div className="error">{errors.name}</div>}
+          
+            <input className="input-field" 
+             type="text" 
+             name="lastName" 
+             value={values.lastName} 
+             placeholder="Enter last name"
+             onChange={handleChange}
+             onBlur={handleBlur} required></input>
+             {touched.lastName && errors.lastName && <div className="error">{errors.lastName}</div>}
+
+            <input className="input-field" 
+             type="text" 
+             name="email" 
+             value={values.email}  
+             placeholder="Enter email"
+             onChange={handleChange}
+             onBlur={handleBlur} required></input>
+             {touched.email && errors.email && <div className="error">{errors.email}</div>}
             
             <input className="input-field" 
-             type="text" name="lastName" 
-             value={initialValues.lastName} 
-             onChange={handleChange} placeholder="Enter password"></input>
+             type="text" 
+             name="password" 
+             value={values.password}  
+             placeholder="Enter password"
+             onChange={handleChange}
+             onBlur={handleBlur} required></input>
+             {touched.password && errors.password && <div className="error">{errors.password}</div>}
+            
+            <input className="input-field" 
+              type="text" 
+              name="repassword" 
+              value={values.repassword}  
+              placeholder="Confirm password"
+              onChange={handleChange}
+              onBlur={handleBlur} required></input>
+              {touched.repassword && errors.repassword && <div className="error">{errors.repassword}</div>}
              
-            <input className="input-field" 
-              type="text" name="lastName" 
-              value={initialValues.lastName} 
-              onChange={handleChange} placeholder="Confirm password"></input>
-            
             <button className="submit-btn" type="submit">Register</button>
         </form>
+      )}  
     </Formik>  
     );
 }
