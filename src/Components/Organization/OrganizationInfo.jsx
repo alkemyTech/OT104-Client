e31e450483066f3c3894c3e42de36447d8f89490
JuanStromanIlz <juanstroman@gmail.com>
2021-11-26 09:50:58 -0300

    now the info of the organization is fetched from the api.

--- src/Components/Organization/OrganizationInfo.jsx
+++ src/Components/Organization/OrganizationInfo.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import styles from './OrganizationInfo.module.css'
 
-function OrganizationInfo({info}) {
+function OrganizationInfo() {
+  const [orgData, setOrgData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        let res = await axios.get('http://ongapi.alkemy.org/api/organization');
+        setOrgData(res.data.data);
+      } catch(err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <img src={info.image} alt="Organization" />
+      <img src={orgData.logo} alt="Organization" />
       <div className={styles.info}>
-        <h1 className={styles.name}>{info.name}</h1>
-        <p className={styles.description}>{info.shortDescription}</p>
+        <h1 className={styles.name}>{orgData.name}</h1>
+        <p className={styles.description}>{orgData.short_description}</p>
         <Link className={styles.button} to="/backoffice/organization/edit">Edit Info</Link>
       </div>
     </div>
