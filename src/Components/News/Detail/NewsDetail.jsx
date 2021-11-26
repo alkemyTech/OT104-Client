fc8978c3440bd5dabcbd7c42a1b746fed79e0cac
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-11-26 11:32:23 -0300

    OTP104-49 Connect with Api
    The NewsDetail component was connected with the api and the news was obtained.

--- src/Components/News/Detail/NewsDetail.jsx
+++ src/Components/News/Detail/NewsDetail.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+function NewsDetail(e) {
+  //   axios.get("http://ongapi.alkemy.org/api/news").then((data) => {
+  //     console.log(data.data.data);
+  //   });
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const getNewsData = async () => {
+      await axios
+        .get("http://ongapi.alkemy.org/api/news")
+        .then((newData) => {
+          setNews((news) => newData.data.data);
+        })
+        .catch((err) => {
+          alert("Error", err);
+        });
+    };
+    getNewsData();
+  }, []);
 
-function NewsDetail() {
-  return <div></div>;
+  return (
+    <div className="news-conatiner">
+      <h1>Title</h1>
+      {console.log(news)}
+    </div>
+  );
 }
 
 export default NewsDetail;
