f7b1ab89649d40124b7ef4484db9503b410889ce
amadeodlp <amadeodlp@hotmail.com>
2021-11-26 20:14:06 -0300

    TestimonialsForm.js first edit. Added validation, CKeditor description and API requests.

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,16 +1,27 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import '../FormStyles.css';
 import * as yup from "yup";
+import { Formik, Form, ErrorMessage, Field } from 'formik';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import axios from 'axios';
 
-const TestimonialForm = () => {
-    const [initialValues, setInitialValues] = useState({
-       name: '',
-       description: '', 
-       image: ''
-    });
+
+const TestimonialForm = ({ testimonial = null }) => {
+
+
+    const [ckEditorError, setCkEditorError] = useState(false);
+    const [message, setMessage] = useState("");
+    const isEditing = !!testimonial;
+
+     const initialValues = {
+    name: testimonial?.name || "",
+    description: testimonial?.description || "",
+    image: testimonial?.image || ""
+  };
 
     const schema = yup.object().shape({
-      name: yup.string().min(4).required("You have to provide a name that has at least 4 characters long."),
+      name: yup.string().min(4, "Name must be at least 4 characters long.").required("You have to provide a name."),
       description: yup.string().required("You have to provide a description."),
       image: yup.string()
       .matches(
@@ -20,30 +31,74 @@ const TestimonialForm = () => {
       .required("You have to provide an image.")
     });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        } if(e.target.name === 'image'){
-            setInitialValues({...initialValues, image: e.target.value})
-    }
-}
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        const isValid = await schema.isValid(initialValues);
-        console.log(isValid);
-    }
-
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Testimonial Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Testimonial description"></input>
-            <input className="input-field" type="file" name="image" value={initialValues.image} onChange={handleChange} placeholder="Testimonial image"></input>
+        <Formik
+        initialValues= {initialValues}
+        validationSchema = {schema}
+        onSubmit={async (values)=> {
+            console.log(values);
+            if (isEditing) {
+            try {
+                await axios.patch(`http://ongapi.alkemy.org/api/testimonials/${testimonial.id}`, values)
+                setMessage("Testimonio editado correctamente");
+                setTimeout(()=>{
+            setMessage("")
+            }, 4000)
+            } catch (error) {
+                console.log(error.response.data);
+                setMessage("Ha habido un error.");
+                setTimeout(()=>{
+                setMessage("")
+                }, 4000)
+            }}
+            try {
+                await axios.post(`http://ongapi.alkemy.org/api/testimonials`, values)
+                .then((response)=>{
+                console.log(response);
+                setMessage("Testimonio editado correctamente");
+                setTimeout(()=>{
+                setMessage("")
+                }, 4000)
+            })
+           } catch (error) {
+                console.log(error.response.data);
+                    setMessage("Ha habido un error.");
+                setTimeout(()=>{
+                setMessage("")
+                }, 4000)
+            }
+         }}
+        >
+        {({values, setFieldValue}) => (
+            <div>
+        <Form className="form-container">
+            <Field 
+            className="input-field" 
+            type="text" 
+            name="name" 
+            placeholder="Testimonial title"/>
+            <ErrorMessage name="name" />
+             <CKEditor
+              className="input-field"
+              name="description"
+              data={values.description}
+              editor={ClassicEditor}
+              onChange={(_, editor) => setFieldValue("description", editor.getData())}
+              onBlur={( _, editor ) => editor.getData() === "" ? setCkEditorError(true) : setCkEditorError(false)}
+            /> 
+            {ckEditorError && <p>Please, write a description.</p>}
+            <Field 
+            className="input-field" 
+            type="file" 
+            name="image" 
+            placeholder="Testimonial image"/>
+           <ErrorMessage name="image" />
             <button className="submit-btn" type="submit">Send</button>
-        </form>
+            {message && <div>{message}</div>}
+        </Form>
+           </div>
+        )}
+        </Formik>
     );
 }
  
