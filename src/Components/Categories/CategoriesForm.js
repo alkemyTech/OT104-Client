8557e7357023e82b7133e2fb9f6518e0807e2ea6
Edgar Britez <edgarfbritez@gmail.com>
2021-11-27 18:28:56 -0300

    fixing some mess on my cade -- refactor

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,116 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import "../FormStyles.css";
-import { ErrorMessage, Formik, Form } from "formik";
+import { Formik, Form, Fild, ErrorMessage, validateYupSchema } from "formik";
 import * as Yup from "yup";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import axios from "axios";
 
-const fakeCategoriy = {
-  name: "category 01",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi velit deleniti molestias illum ab nemo mollitia ad, reiciendis veniam aperiam!",
-  image: "https://picsum.photos/200/300",
-};
-
-const categoriesSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(4, "Too Short!")
-    .max(100, "Must be 15 characters or less")
-    .required("This field is required!"),
-  description: Yup.string()
-    .min(1, "This field is required!")
-    .max(140, "Must be 20 characters or less")
-    .required("This field is required!"),
-  image: Yup.string().required("This field is required!"),
-});
-
 const CategoriesForm = () => {
-  const [image, setImage] = React.useState(null);
   const URL_POST = "http://ongapi.alkemy.org/api/categories";
-  // const URL_PUT = `http://ongapi.alkemy.org/api/categories${category_id}`;
+  const [initialValues, setInitialValues] = useState({
+    name: "Edgar",
+    description: "I am groot",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    if (e.target.name === "name") {
+      setInitialValues({ ...initialValues, name: e.target.value });
+    }
+    if (e.target.name === "description") {
+      setInitialValues({ ...initialValues, description: e.target.value });
+    }
+  };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(initialValues);
+  // };
+
   const convertToBase64Handler = (file) => {
     const reader = new FileReader();
     reader.readAsBinaryString(file);
     reader.onload = function () {
       const base64 = btoa(reader.result);
-      setImage(base64);
-      console.log(base64);
+      setInitialValues((initialValues.image = base64));
     };
     reader.onerror = function (error) {
       console.log("Error: ", error);
     };
   };
 
-  // if user click on create category the path will be /categories/create-category
-  // if user click on edit category the path will be /categories/create-category/:id
+  const validateYupSchema = Yup.object().shape({
+    name: Yup.string()
+      .min(4, "There be at least 4 characters")
+      .required("Name is required"),
+    description: Yup.string()
+      .min(1, "This field can't be empty.")
+      .required("Description is required"),
+  });
 
-  console.log("image from state", image);
   return (
     <Formik
-      initialValues={{ name: "", description: "", categoryImage: image || "" }}
-      validationSchema={categoriesSchema}
+      initialValues={initialValues}
+      validationSchema={validateYupSchema}
       onSubmit={(values, { setSubmitting }) => {
-        console.log("values", values);
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
-          // console.log("values", values);
-          // axios
-          //   .post(URL_POST, JSON.stringify(values))
-          //   .then((res) => {
-          //     console.log(res);
-          //   })
-          //   .catch((err) => {
-          //     console.log(err);
-          //   });
+          let valuesJson = JSON.stringify(values);
+          axios
+            .post(URL_POST, values)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
 
           setSubmitting(false);
         }, 400);
       }}
     >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-      }) => (
-        <Form className="form-container" onSubmit={handleSubmit}>
+      {({ values, handleSubmit, handleBlur, handleChange, handleReset }) => (
+        <form className="form-container" onSubmit={handleSubmit}>
           <input
             className="input-field"
             type="text"
             name="name"
-            value={values.name}
+            value={initialValues.name}
             onChange={handleChange}
             placeholder="Title"
           ></input>
-          {/* To the future >>> this need to be stiled!!! */}
-          {errors.name && touched.name && errors.name}
-
-          <CKEditor
+          <input
+            className="input-field"
+            type="text"
             name="description"
-            editor={ClassicEditor}
-            data={values.description}
-            onReady={(editor) => {
-              // You can store the "editor" and use when it is needed.
-              console.log("Editor is ready to use!", editor);
-            }}
-            onChange={(event, editor) => {
-              const data = editor.getData();
-              // this pass the data to the formik
-              values.description = data;
-            }}
-            onBlur={(event, editor) => {
-              console.log("Blur.", editor);
-            }}
-            onFocus={(event, editor) => {
-              console.log("Focus.", editor);
-            }}
-          />
-          {/* To the future >>> this need to be stiled!!! */}
-          {errors.description && touched.description && errors.description}
+            value={initialValues.description}
+            onChange={handleChange}
+            placeholder="Write some description"
+          ></input>
 
           <input
             type="file"
@@ -125,7 +96,7 @@ const CategoriesForm = () => {
           <button className="submit-btn" type="submit">
             Send
           </button>
-        </Form>
+        </form>
       )}
     </Formik>
   );
