5bba88e96b60447c5fc28e5fcddf463a6a3e7b52
silvioaa <silvio.acevedo@gmail.com>
2021-11-27 15:25:59 -0300

    Creation of reusable form for both creating and editing activities, with name, description and image fields, as well as Formik validation of the name and image fields. The edition of existing activities works well, but the creation doesn't, as the endpoint required in the ticket for the task (activities/create)  doesn't accept the also stiuplated POST method, without prejudging the bugs that the code may also have.

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import '../FormStyles.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { FormikProvider, Form, Field, ErrorMessage, useFormik } from "formik";
 
-const ActivitiesForm = ({activityToEdit}) => {
-    const isCreate = activityToEdit===undefined?false:true;
-    const [initialValues, setInitialValues] = useState(
+const ActivitiesForm = ({
+    activityToEdit
+}) => {
+    const isCreate = activityToEdit===undefined?true:false;
+
+    const fileInput = useRef();
+    const descriptionInput = useRef();
+    const [imageFile, setImageFile ] = useState("");
+    const initialValues = 
         isCreate?
             {
                 name: '',
@@ -13,37 +20,71 @@ const ActivitiesForm = ({activityToEdit}) => {
                 description: ''
             }
         :
-        activityToEdit
-    );
+        activityToEdit;
 
-    const handleChangeNameAndImage = (e) => {
-        if(e.target.name==="name") setInitialValues({...initialValues, name: e.target.value});
-        else if(e.target.name==="image") setInitialValues({...initialValues, image: e.target.value});
+    const handleErrors = ( values ) => {
+        let errors = {}
+        if(!values.name){
+            errors.name = "Por favor, ingrese un nombre."
+        }
+        if(!values.image){
+            errors.image = "Por favor, ingrese una imagen."
+        }
+
+        return errors;
     }
 
-    const handleChangeDescription = (event, editor) => {
-            const data = editor.getData();
-            setInitialValues({...initialValues, description: data})
-    }
-
-    const handleSubmit = (e) => {
+    function handleFileChange(e){
         e.preventDefault();
-        if(initialValues.name===""){
-            alert("Por favor, introduzca un nombre para la actividad.");
-            return
+        if(e.target.files[0]!==undefined){
+            const reader = new FileReader();
+            reader.readAsDataURL(e.target.files[0]);
+            reader.onload = () => {
+                formik.setFieldValue("image",reader.result, true);
+                formik.validateField("image");
+            };
+            reader.onerror = (error)=>{
+                fileInput.current.value = "";
+                formik.setFieldValue("image","",true);
+                formik.validateField("image")
+                alert("No se pudo cargar el archivo de imagen. Error:" + error + ".");
+            }
+        }else{
+            formik.setFieldValue("image","", true);
+            formik.validateField("image");
         }
-        if(initialValues.description===""){
-            alert("Por favor, introduzca una descripción para la actividad.");
-            return
+    }
+
+    function handleDescriptionChange(__,editor){
+        const data = editor.getData();
+    }
+
+    function handleDescriptionBlur(e){
+        e.target = {};
+        e.target.name="description";
+        formik.handleBlur(e);
+    } 
+
+    const handleSubmit = async (values, { resetForm }) => {
+        const now = new Date();
+
+        let fetchBody = {
+            name: values.name,
+            slug:null,
+            image:values.image,
+            description:values.description,
+            created_at: isCreate ? now.toISOString() : initialValues.created_at,
+            updated_at:now.toISOString(),
+            id:isCreate ? null : activityToEdit.id,
+            user_id:isCreate ? null : activityToEdit.user_id,
+            category_id:isCreate ? null : activityToEdit.category_id,
+            deleted_at:isCreate ? null : activityToEdit.deleted_at
         }
+
         try{
-            fetch(`http://apiurl/activities/${isCreate?"create":initialValues.id}`,{
-                method:isCreate ? "patch":"post",
-                body:JSON.stringify({
-                    name: initialValues.name,
-                    file:initialValues.file,
-                    description:initialValues.description
-                }),
+            fetch(`http://ongapi.alkemy.org/api/activities/${isCreate?"create":initialValues.id}`,{
+                method:isCreate ? "post":"put",
+                body:JSON.stringify(fetchBody),
                 headers: {
                     'Content-type': 'application/json',
                 }
@@ -55,39 +96,70 @@ const ActivitiesForm = ({activityToEdit}) => {
                     return res.json();
                 }
             })
-            .then((data)=>`La actividad fue${isCreate?"creada":"modificada"} satisfactoriamente.`)
+            .catch((error)=>console.error(error))
+            .then((data)=>{
+                alert(`La actividad fue${isCreate?"creada":"modificada"} satisfactoriamente.`);
+                if(isCreate===true) resetForm();
+            })
         }catch(error){
-            alert(`No se pudo modificar la actividad. Error ${error.error}`)
+            alert(`No se pudo ${isCreate?"crear":"modificar"} la actividad. Error ${error.error}`)
         }
     }
+
+    const formik = useFormik(
+        {
+            initialValues: initialValues,
+            onSubmit: handleSubmit,
+            validate: handleErrors
+        }
+    )
     
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input 
-                className="input-field" 
-                type="text" 
-                name="name" 
-                value={initialValues.name} 
-                onChange={handleChangeNameAndImage} 
-                placeholder="Activity Title"
-            />
-            <input 
-                className="input-field" 
-                value="initialValues.image"
-                type="file"
-                name="image"
-                onChange={handleChangeNameAndImage} 
-                accept="image/png, image/jpeg"
-            />
-             <CKEditor
-                className="input-field" 
-                id="description"
-                editor={ ClassicEditor }
-                data={initialValues.description}
-                onChange={handleChangeDescription}
-            />
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <FormikProvider value={formik}>  
+            <Form className="form-container" encType="multipart/form-data">
+                <div>
+                    <Field
+                        type="text" 
+                        name="name" 
+                        value={formik.values.name}
+                        placeholder="Activity Title"
+                        onChange={(e)=>{
+                            formik.handleChange(e)
+                            formik.validateField("name")
+                        }}
+                    />
+                    <ErrorMessage 
+                        name="name" 
+                        component={()=><span>{formik.errors.name}</span>}
+                    />
+                </div>
+                <div>
+                    <input 
+                        type="file"
+                        name="image"
+                        onChange={handleFileChange} 
+                        accept="image/png, image/jpeg"
+                        ref={fileInput}
+                        onBlur={formik.handleBlur}
+                    />
+                    <ErrorMessage 
+                        name="image" 
+                        component={()=><span>{formik.errors.image}</span>}
+                    />
+                </div>
+                <div>
+                    <CKEditor
+                        id="description"
+                        editor={ ClassicEditor }
+                        data={formik.values.description}
+                        ref={descriptionInput}
+                        onChange={handleDescriptionChange}
+                        onBlur={handleDescriptionBlur}
+                    />
+                </div>
+                <button type="submit">Send</button>
+            </Form> 
+        </FormikProvider>
     );
 }
  
