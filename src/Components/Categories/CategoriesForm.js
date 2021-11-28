c9e2bc52913745fdc6add234d52314ec0e32a9ab
Edgar Britez <edgarfbritez@gmail.com>
2021-11-27 22:41:21 -0300

    At last... The image is saved and the server response Ok :)

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -6,36 +6,26 @@ import axios from "axios";
 
 const CategoriesForm = () => {
   const URL_POST = "http://ongapi.alkemy.org/api/categories";
+  // const [preview, setPreview] = useState(null);
+  const [imageState, setImageState] = useState("");
   const [initialValues, setInitialValues] = useState({
     name: "Edgar",
     description: "I am groot",
     image: "",
   });
 
-  const handleChange = (e) => {
-    if (e.target.name === "name") {
-      setInitialValues({ ...initialValues, name: e.target.value });
+  const convertToBase64Handler = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        // setPreview(reader.result);
+        let file64 = reader.result;
+        console.log(typeof file64);
+        setImageState(file64);
+      };
     }
-    if (e.target.name === "description") {
-      setInitialValues({ ...initialValues, description: e.target.value });
-    }
-  };
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log(initialValues);
-  // };
-
-  const convertToBase64Handler = (file) => {
-    const reader = new FileReader();
-    reader.readAsBinaryString(file);
-    reader.onload = function () {
-      const base64 = btoa(reader.result);
-      setInitialValues((initialValues.image = base64));
-    };
-    reader.onerror = function (error) {
-      console.log("Error: ", error);
-    };
   };
 
   const validateYupSchema = Yup.object().shape({
@@ -48,57 +38,58 @@ const CategoriesForm = () => {
   });
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validateYupSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          let valuesJson = JSON.stringify(values);
-          axios
-            .post(URL_POST, values)
-            .then((res) => console.log(res))
-            .catch((err) => console.log(err));
+    <React.Fragment>
+      {/* {preview && <img width="300" height="400" src={preview} alt="preview" />} */}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validateYupSchema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            axios
+              .post(URL_POST, values)
+              .then((res) => console.log(res))
+              .catch((err) => console.log(err));
 
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({ values, handleSubmit, handleBlur, handleChange, handleReset }) => (
-        <form className="form-container" onSubmit={handleSubmit}>
-          <input
-            className="input-field"
-            type="text"
-            name="name"
-            value={initialValues.name}
-            onChange={handleChange}
-            placeholder="Title"
-          ></input>
-          <input
-            className="input-field"
-            type="text"
-            name="description"
-            value={initialValues.description}
-            onChange={handleChange}
-            placeholder="Write some description"
-          ></input>
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({ values, handleSubmit, handleBlur, handleChange, handleReset }) => (
+          (values.image = imageState),
+          (
+            <form className="form-container" onSubmit={handleSubmit}>
+              <input
+                className="input-field"
+                type="text"
+                name="name"
+                value={values.name}
+                onChange={handleChange}
+                placeholder="Title"
+              ></input>
+              <input
+                className="input-field"
+                type="text"
+                name="description"
+                value={values.description}
+                onChange={handleChange}
+                placeholder="Write some description"
+              ></input>
 
-          <input
-            type="file"
-            name="categoryImage"
-            accept=".jpg,.jpeg,.png"
-            onChange={(event) =>
-              (values.categoryImage = convertToBase64Handler(
-                event.target.files[0]
-              ))
-            }
-          />
-          <button className="submit-btn" type="submit">
-            Send
-          </button>
-        </form>
-      )}
-    </Formik>
+              <input
+                type="file"
+                name="categoryImage"
+                accept=".jpg,.jpeg,.png"
+                onChange={convertToBase64Handler}
+              />
+              <button className="submit-btn" type="submit">
+                Send
+              </button>
+            </form>
+          )
+        )}
+      </Formik>
+    </React.Fragment>
   );
 };
 
