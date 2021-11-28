ae34a276c8f1e4e9ebe6d93ddda11dd5b25bab97
Edgar Britez <edgarfbritez@gmail.com>
2021-11-28 11:29:36 -0300

    create and edit component base on url

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,26 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
+import { useParams } from "react-router-dom";
 import "../FormStyles.css";
-import { Formik, Form, Fild, ErrorMessage, validateYupSchema } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+// README!!!!!
+// I create a route to de categories edit page and pass the id as a parameter
+// "/create-category/:categoryId"
+// If I get a param id, I will get the category and set the initial values
+// If I don't get a param id, I will create a new category
 
 const CategoriesForm = () => {
-  const URL_POST = "http://ongapi.alkemy.org/api/categories";
-  // const [preview, setPreview] = useState(null);
-  const [imageState, setImageState] = useState("");
-  const [initialValues, setInitialValues] = useState({
-    name: "Edgar",
-    description: "I am groot",
+  const BASE_URL = "http://ongapi.alkemy.org/api/categories";
+  const { categoryId } = useParams();
+  const [category, setCategory] = React.useState({});
+  const [imageState, setImageState] = React.useState("");
+  const [initialValues, setInitialValues] = React.useState({
+    name: "",
+    description: "",
     image: "",
   });
-
   const convertToBase64Handler = (e) => {
     const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onloadend = () => {
-        // setPreview(reader.result);
         let file64 = reader.result;
         console.log(typeof file64);
         setImageState(file64);
@@ -37,60 +45,103 @@ const CategoriesForm = () => {
       .required("Description is required"),
   });
 
-  return (
-    <React.Fragment>
-      {/* {preview && <img width="300" height="400" src={preview} alt="preview" />} */}
+  const ConditionalForm = () => {
+    return (
       <Formik
         initialValues={initialValues}
         validationSchema={validateYupSchema}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            axios
-              .post(URL_POST, values)
-              .then((res) => console.log(res))
-              .catch((err) => console.log(err));
+            if (category.id) {
+              axios
+                .put(`${BASE_URL}/${category.id}`, values)
+                .then((res) => alert("Category updated successfully"))
+                .catch((err) => console.log("PUT method ERROR: ", err));
+            } else {
+              axios
+                .post(BASE_URL, values)
+                .then((res) => alert("Category created successfully"))
+                .catch((err) => console.log("POST method ERROR: ", err));
+            }
 
             setSubmitting(false);
           }, 400);
         }}
       >
-        {({ values, handleSubmit, handleBlur, handleChange, handleReset }) => (
+        {({ values, errors, touched }) => (
           (values.image = imageState),
           (
-            <form className="form-container" onSubmit={handleSubmit}>
-              <input
+            <Form className="form-container">
+              <Field
                 className="input-field"
-                type="text"
                 name="name"
-                value={values.name}
-                onChange={handleChange}
-                placeholder="Title"
-              ></input>
-              <input
-                className="input-field"
                 type="text"
+                placeholder="Title"
+                setFieldValue={category.name}
+              />
+              {errors.name && <ErrorMessage name="name" />}
+              <CKEditor
                 name="description"
-                value={values.description}
-                onChange={handleChange}
-                placeholder="Write some description"
-              ></input>
+                editor={ClassicEditor}
+                data={values.description}
+                onReady={(editor) => {
+                  // You can store the "editor" and use when it is needed.
+                  console.log("Editor is ready to use!", editor);
+                }}
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  // this pass the data to the formik
+                  values.description = data;
+                }}
+                onBlur={(event, editor) => {
+                  console.log("Blur.", editor);
+                }}
+                onFocus={(event, editor) => {
+                  console.log("Focus.", editor);
+                }}
+              />
+              {errors.description &&
+                touched.description &&
+                errors.description && <ErrorMessage name="description" />}
 
-              <input
+              <Field
                 type="file"
-                name="categoryImage"
+                name="image"
                 accept=".jpg,.jpeg,.png"
                 onChange={convertToBase64Handler}
               />
+              {errors.image && touched.image && errors.image && (
+                <ErrorMessage name="image" />
+              )}
+
               <button className="submit-btn" type="submit">
                 Send
               </button>
-            </form>
+            </Form>
           )
         )}
       </Formik>
-    </React.Fragment>
-  );
+    );
+  };
+
+  React.useEffect(() => {
+    if (categoryId) {
+      axios
+        .get(`${BASE_URL}/${categoryId}`)
+        .then((res) => {
+          let data = res.data.data;
+          setInitialValues({ ...data });
+          setCategory(data);
+        })
+        .catch((err) => {
+          console.log("Error getting the category", err);
+        });
+    }
+  }, [categoryId]);
+
+  React.useEffect(() => {}, [initialValues]);
+
+  return initialValues.id ? <ConditionalForm /> : <ConditionalForm />;
 };
 
 export default CategoriesForm;
