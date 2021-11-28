63e8223ddcb9d987f1506a8c9516b12e077f4c93
amadeodlp <amadeodlp@hotmail.com>
2021-11-28 14:12:37 -0300

    minor fixes

--- src/Components/Members/MembersForm.js
+++ src/Components/Members/MembersForm.js
@@ -9,17 +9,17 @@ import axios from 'axios';
 const MembersForm = ({member = null}) => {
   
   const [ckEditorError, setCkEditorError] = useState(false);
-    const [message, setMessage] = useState("");
-    const isEditing = !!member;
+  const [message, setMessage] = useState("");
+  const isEditing = !!member;
 
-    const initialValues = {
-    name: member?.name || "",
-    image: member?.image || "",
-    description: member?.description || "",
-    facebook: member?.facebook || "",
-    instagram: member?.instagram || "",
-    linkedin: member?.linkedin || ""
-  };
+  const initialValues = {
+        name: member?.name || "",
+        image: member?.image || "",
+        description: member?.description || "",
+        facebook: member?.facebook || "",
+        instagram: member?.instagram || "",
+        linkedin: member?.linkedin || ""   
+    };
 
   const schema = yup.object().shape({
       name: yup.string().min(4, "Name must be at least 4 characters long.").required("You have to provide a name."),
@@ -55,7 +55,6 @@ const MembersForm = ({member = null}) => {
         initialValues= {initialValues}
         validationSchema = {schema}
         onSubmit={async (values)=> {
-            console.log(values);
             if (isEditing) {
             try {
                 await axios.patch(`http://ongapi.alkemy.org/api/members/${member.id}`, values)
@@ -89,51 +88,51 @@ const MembersForm = ({member = null}) => {
          }}
         >
         {({values, setFieldValue}) => (
-            <div>
-        <Form className="form-container">
+        <div>
+          <Form className="form-container">
+              <Field 
+              className="input-field" 
+              type="text" 
+              name="name" 
+              placeholder="Member name"/>
+                <ErrorMessage name="name" />
+              <Field 
+               className="input-field" 
+               type="file" 
+               name="image" 
+               placeholder="Member image"/>
+                   <ErrorMessage name="image" />
+              <CKEditor
+                className="input-field"
+                name="description"
+               data={values.description}
+               editor={ClassicEditor}
+               onChange={(_, editor) => setFieldValue("description", editor.getData())}
+                  onBlur={( _, editor ) => editor.getData() === "" ? setCkEditorError(true) : setCkEditorError(false)}
+              /> 
+                  {ckEditorError && <p>Please, write a description.</p>}
+              <Field 
+             className="input-field" 
+             type="text" 
+             name="facebook" 
+              placeholder="Facebook profile"/>
+                   <ErrorMessage name="facebook" />
             <Field 
-            className="input-field" 
-            type="text" 
-            name="name" 
-            placeholder="Member name"/>
-            <ErrorMessage name="name" />
-            <Field 
-            className="input-field" 
-            type="file" 
-            name="image" 
-            placeholder="Member image"/>
-           <ErrorMessage name="image" />
-             <CKEditor
-              className="input-field"
-              name="description"
-              data={values.description}
-              editor={ClassicEditor}
-              onChange={(_, editor) => setFieldValue("description", editor.getData())}
-              onBlur={( _, editor ) => editor.getData() === "" ? setCkEditorError(true) : setCkEditorError(false)}
-            /> 
-            {ckEditorError && <p>Please, write a description.</p>}
-            <Field 
-            className="input-field" 
-            type="text" 
-            name="facebook" 
-            placeholder="Facebook profile"/>
-            <ErrorMessage name="facebook" />
-            <Field 
-            className="input-field" 
-            type="text" 
-            name="instagram" 
-            placeholder="Instagram profile"/>
-            <ErrorMessage name="instagram" />
-            <Field 
-            className="input-field" 
-            type="text" 
-            name="linkedin" 
-            placeholder="Linkedin profile"/>
-            <ErrorMessage name="linkedin" />
-            <button className="submit-btn" type="submit">Send</button>
-            {message && <div>{message}</div>}
-        </Form>
-           </div>
+               className="input-field" 
+               type="text" 
+               name="instagram" 
+               placeholder="Instagram profile"/>
+                    <ErrorMessage name="instagram" />
+               <Field 
+               className="input-field" 
+               type="text" 
+               name="linkedin" 
+               placeholder="Linkedin profile"/>
+                   <ErrorMessage name="linkedin" />
+               <button className="submit-btn" type="submit">Send</button>
+                {message && <div>{message}</div>}
+            </Form>
+        </div>
         )}
         </Formik>
     );
