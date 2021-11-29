9a8203d5e8584800ef52280e7fa9122f1142b7ab
unknown <janoncarlos@gmail.com>
2021-11-29 11:28:59 -0300

    unified form TODO check imput data

--- src/Components/Admin/HomeForm.jsx
+++ src/Components/Admin/HomeForm.jsx
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import { Form, Formik, Field, ErrorMessage } from 'formik'
-// import axios from 'axios';
-// import './homeForm.css';
-// import * as Yup from 'yup';
-// import SlideForm from './SlideForm';
-// import WelcomeTextForm from './WelcomeTextForm';
-
-// const HomeForm = () => {
-//   return (
-//     <div className="formContainer">
-//       <h3 className="slideLabel">Welcome Text</h3>
-//       <WelcomeTextForm />
-//       <h3 className="slideLabel">Slide 1</h3>
-//       <SlideForm />
-//       <h3 className="slideLabel">Slide 2</h3>
-//       <SlideForm />
-//       <h3 className="slideLabel">Slide 3</h3>
-//       <SlideForm />
-//     </div>
-//   )
-// }
-
-// export default HomeForm
-
-
 import React from 'react'
 import { Form, Formik, Field, ErrorMessage } from 'formik'
 import axios from 'axios';
