56690867fca15ce33c60847281f4cdc999da02a8
Ignacio Hardoy <hartoyy@hotmail.com>
2021-11-30 16:55:23 -0300

    boostrap added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
@@ -13,6 +14,8 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import RegisterForm from './Components/Auth/RegisterForm';
+
 
 function App() {
   return (
@@ -30,6 +33,7 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
+          <Route path="/registerform" component={RegisterForm} />
         </Switch>
       </BrowserRouter>
     <div className="App">
--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import '../FormStyles.css';
 import { Formik } from 'formik';
+import { Container, Row, Form, Button } from 'react-bootstrap';
+
 
 const RegisterForm = () => {
     
@@ -14,43 +15,47 @@ const RegisterForm = () => {
         password:"",
         repassword:""
       }}
+      
+
 
       validate={(values)=>{
-        let errores = {};
+        let errors = {};
   
         //Validacion nombre
         if(!values.name){
-          errores.name = "Por favor ingresa un nombre"
+          errors.name = "Por favor ingresa un nombre"
         }
   
         //Validacion last nambre
         if(!values.lastName){
-          errores.lastName = "Por favor ingresa un apellido"
+          errors.lastName = "Por favor ingresa un apellido"
         }
         
         //Validacion email
         if(!values.email){
-            errores.email = "Por favor ingresa un email"
+            errors.email = "Por favor ingresa un email"
           }else if  (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(values.email)){
-            errores.password = "El email solo puede contener letras, numeros, puntos, guiones y guion bajo"
+            errors.password = "El email solo puede contener letras, numeros, puntos, guiones y guion bajo"
           }
 
         //Validacion password
         if(!values.password){
-            errores.password = "Por favor ingresa una contraseña"
+            errors.password = "Por favor ingresa una contraseña"
             console.log(values.password);
           }else if  (!/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/.test(values.password)){
-            errores.password = "La contraseña debe tener una longitud mínima de 6 caraceteres, y contener al menos un número, una letra y un símbolo"
+            errors.password = "La contraseña debe tener una longitud mínima de 6 caraceteres, y contener al menos un número, una letra y un símbolo"
           }
           
 
         //Validacion repassword
-        if(values.repassword !== values.password){
-            errores.repassword = "Las contraseñas no coinciden"
+        if(!values.repassword){
+            errors.repassword = "Por favor ingresa confirma tu contraseña"
+          }else if (values.repassword !== values.password){
+            errors.repassword = "Las contraseñas no coinciden"
           }  
 
 
-        return errores;
+        return errors;
       }}
 
       onSubmit={(values, {resetForm}) =>{
@@ -60,62 +65,91 @@ const RegisterForm = () => {
           email:values.email,
           password:values.password
       };
-         console.log(userInfo);
+         
         resetForm();     
       }}
      >
-      {( {values, errors, touched, handleSubmit, handleChange, handleBlur} )=>(   
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" 
-             type="text" 
-             name="name" 
-             value={values.name} 
-             placeholder="Enter name"
-             onChange={handleChange}
-             onBlur={handleBlur} required></input>
-             {touched.name && errors.name && <div className="error">{errors.name}</div>}
-          
-            <input className="input-field" 
-             type="text" 
-             name="lastName" 
-             value={values.lastName} 
-             placeholder="Enter last name"
-             onChange={handleChange}
-             onBlur={handleBlur} required></input>
-             {touched.lastName && errors.lastName && <div className="error">{errors.lastName}</div>}
+      {( {values, errors, touched, isValid, handleSubmit, handleChange, handleBlur} )=>(
+  <Container>
+    <Row>   
+    <h2 className="mt-3 text-center">Register Form</h2>
+      <Form className="border border-grey rounded p-4 mt-4" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Control 
+            type="text" 
+            name="name" 
+            value={values.name} 
+            placeholder="Enter name"
+            onChange={handleChange}
+            onBlur={handleBlur} required
+            isValid={touched.name && !errors.name}
+            isInvalid={touched.name && errors.name}/>
+        </Form.Group>
+        {touched.name && errors.name && <div className="text-danger mb-3">{errors.name}</div>}
+      
+        <Form.Group className="mb-3">
+          <Form.Control 
+            type="text" 
+            name="lastName" 
+            value={values.lastName} 
+            placeholder="Enter last name"
+            onChange={handleChange}
+            onBlur={handleBlur} required 
+            isValid={touched.lastName && !errors.lastName}
+            isInvalid={touched.lastName && errors.lastName}/>
+        </Form.Group>
+        {touched.lastName && errors.lastName && <div className="text-danger mb-3">{errors.lastName}</div>}
+      
+        <Form.Group className="mb-3">
+          <Form.Control 
+            type="text" 
+            name="email" 
+            value={values.email}  
+            placeholder="Enter email"
+            onChange={handleChange}
+            onBlur={handleBlur} required 
+            isValid={touched.email && !errors.email}
+            isInvalid={touched.email && errors.email}/>
+        </Form.Group>
+        {touched.email && errors.email && <div className="text-danger mb-3">{errors.email}</div>}
+      
+        <Form.Group className="mb-3">
+          <Form.Control 
+            type="text" 
+            name="password" 
+            value={values.password}  
+            placeholder="Enter password"
+            onChange={handleChange}
+            onBlur={handleBlur} required 
+            isValid={touched.password && !errors.password}
+            isInvalid={touched.password && errors.password}/>
+        </Form.Group>
+        {touched.password && errors.password && <div className="text-danger mb-3">{errors.password}</div>}
+      
+        <Form.Group className="mb-3">
+          <Form.Control 
+            type="text" 
+            name="repassword" 
+            value={values.repassword}  
+            placeholder="Confirm password"
+            onChange={handleChange}
+            onBlur={handleBlur} required 
+            isValid={touched.repassword && !errors.repassword}
+            isInvalid={touched.repassword && errors.repassword}/>
+        </Form.Group>
+        {touched.repassword && errors.repassword && <div className="text-danger mb-3">{errors.repassword}</div>}
 
-            <input className="input-field" 
-             type="text" 
-             name="email" 
-             value={values.email}  
-             placeholder="Enter email"
-             onChange={handleChange}
-             onBlur={handleBlur} required></input>
-             {touched.email && errors.email && <div className="error">{errors.email}</div>}
-            
-            <input className="input-field" 
-             type="text" 
-             name="password" 
-             value={values.password}  
-             placeholder="Enter password"
-             onChange={handleChange}
-             onBlur={handleBlur} required></input>
-             {touched.password && errors.password && <div className="error">{errors.password}</div>}
-            
-            <input className="input-field" 
-              type="text" 
-              name="repassword" 
-              value={values.repassword}  
-              placeholder="Confirm password"
-              onChange={handleChange}
-              onBlur={handleBlur} required></input>
-              {touched.repassword && errors.repassword && <div className="error">{errors.repassword}</div>}
-             
-            <button className="submit-btn" type="submit">Register</button>
-        </form>
+        <Button className="submit-btn" type="submit">
+          Register
+        </Button>
+      </Form>
+    </Row>
+  </Container>
       )}  
     </Formik>  
     );
 }
  
 export default RegisterForm;
+
+
