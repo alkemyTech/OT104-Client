08c4a843f67f0e376c516cf8b60e60bef83bbb92
Edgar Britez <edgarfbritez@gmail.com>
2021-11-30 22:46:12 -0300

    add a little React BS

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,24 +1,19 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import "../FormStyles.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import Button from "react-bootstrap/Button";
 
 // React Bootstrap
 // import { Form } from "react-bootstrap/Button";
-
-// README!!!!!
-// I create a route to de categories edit page and pass the id as a parameter
-// "/create-category/:categoryId"
-// If I get a param id, I will get the category and set the initial values
-// If I don't get a param id, I will create a new category
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
 
 const CategoriesForm = ({ cateroryToEdit }) => {
-  console.log(cateroryToEdit);
   const BASE_URL = "http://ongapi.alkemy.org/api/categories";
   const [category, setCategory] = React.useState({});
   const [imageState, setImageState] = React.useState(null);
@@ -79,44 +74,59 @@ const CategoriesForm = ({ cateroryToEdit }) => {
           }, 400);
         }}
       >
-        {({ values, errors, touched }) => (
+        {({
+          values,
+          errors,
+          isValid,
+          touched,
+          handleSubmit,
+          handleChange,
+          handleBlur,
+        }) => (
           (values.image = imageState),
           (
-            <Form className="form-container">
-              <Field
-                className="input-field"
-                name="name"
-                type="text"
-                placeholder="Title"
-                setFieldValue={category.name}
-              />
+            <Form noValidate onSubmit={handleSubmit} className="form-container">
+              <Form.Label>Name</Form.Label>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  className="input-field"
+                  name="name"
+                  type="text"
+                  placeholder="Name"
+                  setFieldValue={category.name}
+                />
 
-              {errors.name && <ErrorMessage name="name" />}
-              <CKEditor
-                name="description"
-                editor={ClassicEditor}
-                data={values.description}
-                onReady={(editor) => {
-                  // You can store the "editor" and use when it is needed.
-                  console.log("Editor is ready to use!", editor);
-                }}
-                onChange={(event, editor) => {
-                  const data = editor.getData();
-                  // this pass the data to the formik
-                  values.description = data;
-                }}
-                onBlur={(event, editor) => {
-                  console.log("Blur.", editor);
-                }}
-                onFocus={(event, editor) => {
-                  console.log("Focus.", editor);
-                }}
-              />
-              {errors.description &&
-                touched.description &&
-                errors.description && <ErrorMessage name="description" />}
+                {errors.name && <ErrorMessage name="name" />}
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Description</Form.Label>
+                <CKEditor
+                  name="description"
+                  editor={ClassicEditor}
+                  data={values.description}
+                  onReady={(editor) => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log("Editor is ready to use!", editor);
+                  }}
+                  onChange={(event, editor) => {
+                    const data = editor.getData();
+                    // this pass the data to the formik
+                    values.description = data;
+                  }}
+                  onBlur={(event, editor) => {
+                    console.log("Blur.", editor);
+                  }}
+                  onFocus={(event, editor) => {
+                    console.log("Focus.", editor);
+                  }}
+                />
+                {errors.description &&
+                  touched.description &&
+                  errors.description && <ErrorMessage name="description" />}
+              </Form.Group>
 
               <input
+                className="input-field"
                 type="file"
                 name="image"
                 accept=".jpg,.jpeg,.png"
