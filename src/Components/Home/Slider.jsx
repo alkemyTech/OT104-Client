ff52d52f42da3cd9d06f8a0258185f762bdc7664
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-12-01 16:56:16 -0300

    Add bootstrap in slider component
    Add new styles and bootstrap

--- src/Components/Home/Slider.jsx
+++ src/Components/Home/Slider.jsx
@@ -1,24 +1,28 @@
 import React from "react";
-import "./styles/sliderStyle.css";
+
 import slide1 from "./img/photo1.jpg";
 import slide2 from "./img/photo2.jpg";
 import slide3 from "./img/photo3.jpg";
 import slide4 from "./img/photo4.jpg";
-import slide5 from "./img/photo5.jpg";
+
+import { Carousel } from "react-bootstrap";
 
 function Slider() {
   return (
-    <>
-      <div id="slider">
-        <figure>
-          <img src={slide1} alt="slide 1" />
-          <img src={slide2} alt="slide 2" />
-          <img src={slide3} alt="slide 3" />
-          <img src={slide4} alt="slide 4" />
-          <img src={slide5} alt="slide 5" />
-        </figure>
-      </div>
-    </>
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slide1} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slide2} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slide3} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slide4} alt="First slide" />
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
