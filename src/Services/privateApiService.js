58222e934d3e896d003a5d2971c3b2d8c9202d0e
FlorAbril <florenciasoto801@gmail.com>
2021-11-30 22:06:27 -0300

    OT104-69 Create VerifyToken method

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -12,4 +12,17 @@ const Get = () => {
     .catch(err => console.log(err))
 }
 
+// Method to verify if the token is in the localStorage and return a header with the token
+const VerifyToken = () => {
+    const token = localStorage.getItem('token');
+    if(token) {
+        const header = {
+            'Authorization': `Bearer ${token}`
+        }
+        return header
+    }
+    return null
+}
+
+export {Verify}
 export default Get
