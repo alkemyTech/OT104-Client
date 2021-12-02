0c831903ee04a605b321c53de90d1d07c3b4025c
JuanStromanIlz <juanstroman@gmail.com>
2021-12-02 10:55:15 -0300

    a new reusable method was created to make PUT request

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
-
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 1 /*Aqui va el ID del equipo!!*/,
+  },
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get('https://jsonplaceholder.typicode.com/users', config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+const Put = async (url, id, body) => {
+  try {
+    const config = {
+      // here we need to call the method to get the token.
+      // headers: { Authorization: `Bearer ${token}` },
+    };
+    let res = await axios.put(`${url}/${id}`, body, { config });
+    return res;
+  } catch (err) {
+    throw Error(err.message);
+  }
+};
+
+export { Get, Put };
