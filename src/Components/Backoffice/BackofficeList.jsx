2219c22cf62915781d0a1a790e90a7e3963db6cf
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-12-02 19:30:34 -0300

    Add link
    Add link to '/backoffice/users/create'

--- src/Components/Backoffice/BackofficeList.jsx
+++ src/Components/Backoffice/BackofficeList.jsx
@@ -4,6 +4,8 @@ import { Spinner } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import { Table } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 function BackofficeList() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -17,41 +19,44 @@ function BackofficeList() {
   }, []);
 
   return (
-    <Container>
+    <Container className="d-flex justify-content-center flex-wrap">
+      <Container className="m-1 text-center">
+        <Link to="/backoffice/users/create">
+          <Button>Crear Usuario</Button>
+        </Link>
+      </Container>
       {users.length === 0 ? (
         <Spinner animation="border" />
       ) : (
-        <Table responsive>
-          <thead>
-            <tr>
-              <th className="text-center">ID</th>
-              <th className="text-center">Nombre</th>
-              <th className="text-center">Email</th>
-              <th className="text-center">Role Id</th>
-              <th className="text-center">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((data, index) => {
-              return (
-                <tr key={index}>
-                  <td className="text-center">{data.id}</td>
-                  <td className="text-center">{data.name}</td>
-                  <td className="text-center">{data.email}</td>
-                  <td className="text-center">{data.role_id}</td>
-                  <td className="text-center">
-                    <Container>
-                      <Button variant="primary" className="m-1">
-                        Editar
-                      </Button>
-                      <Button variant="danger">Eliminar</Button>
-                    </Container>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+        <>
+          <Table>
+            <thead>
+              <tr>
+                <th className="text-center">Nombre</th>
+                <th className="text-center">Email</th>
+                <th className="text-center">Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((data, index) => {
+                return (
+                  <tr key={index}>
+                    <td className="text-center">{data.name}</td>
+                    <td className="text-center">{data.email}</td>
+                    <td className="text-center">
+                      <Container>
+                        <Button variant="primary" className="m-1">
+                          Editar
+                        </Button>
+                        <Button variant="danger">Eliminar</Button>
+                      </Container>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </>
       )}
     </Container>
   );
