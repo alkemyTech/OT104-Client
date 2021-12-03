483647222e9d361001f402c60e45673265864d25
Edgar Britez <edgarfbritez@gmail.com>
2021-12-03 18:50:23 -0300

    adding routes for /donar /gracias. Also the import bootstrap in index.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
- import About from './Components/About/About';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import ActivitiesForm from "./Components/Activities/ActivitiesForm";
+import CategoriesForm from "./Components/Categories/CategoriesForm";
+import NewsForm from "./Components/News/NewsForm";
+import SlidesForm from "./Components/Slides/SlidesForm";
+import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
+import UserForm from "./Components/Users/UsersForm";
+import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
+import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
+import MembersForm from "./Components/Members/MembersForm";
+import ProjectsForm from "./Components/Projects/ProjectsForm";
+import About from "./Components/About/About";
+
+// Donations
+import Donation from "./Components/Donations/Donation";
+import Thanks from "./Components/Donations/Thanks";
 
 function App() {
   return (
@@ -32,55 +36,61 @@ function App() {
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
           <Route path="/Nosotros" component={About} />
+
+          {/* Donations */}
+          <Route path="/donar">
+            <Donation text={"donation"} />
+          </Route>
+          <Route path="/gracias" component={Thanks} />
         </Switch>
       </BrowserRouter>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Counter />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <span>
+            <span>Learn </span>
+            <a
+              className="App-link"
+              href="https://reactjs.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux
+            </a>
+            <span>, </span>
+            <a
+              className="App-link"
+              href="https://redux-toolkit.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Redux Toolkit
+            </a>
+            ,<span> and </span>
+            <a
+              className="App-link"
+              href="https://react-redux.js.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              React Redux
+            </a>
+          </span>
+        </header>
+      </div>
     </>
   );
 }
--- src/Components/Donations/Donation.js
+++ src/Components/Donations/Donation.js
@@ -1,8 +1,22 @@
 import React from "react";
 // import styles from './Donation.module.css';
 
-function Donation() {
-  return <div>Donation Works!</div>;
+function Donation(props) {
+  return <div>{props.text}</div>;
 }
 
+Donation.propTypes = {
+  text(props, propName, componentName) {
+    if (props[propName] === undefined) {
+      return new Error(
+        `You need to pass a prop "${propName}:{'a message as string'}" for ${componentName} Component`
+      );
+    }
+    if (typeof props[propName] !== "string") {
+      return new Error(
+        `You need to pass a string as a prop for ${componentName} Component`
+      );
+    }
+  },
+};
 export default Donation;
