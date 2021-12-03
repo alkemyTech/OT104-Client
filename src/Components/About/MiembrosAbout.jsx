7146c7c6eff7cb1653d2784201ab75971bb2ae75
unknown <janoncarlos@gmail.com>
2021-12-03 17:47:31 -0300

    data w useEffect TODO style

--- src/Components/About/MiembrosAbout.jsx
+++ src/Components/About/MiembrosAbout.jsx
@@ -1,79 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 import Card from 'react-bootstrap/Card';
-import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 
 
-const MiembrosAbout = ({ data }) => {
+const MiembrosAbout = () => {
+  const [aboutText, setAboutText] = useState([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get(`http://ongapi.alkemy.org/api/members`)
+      setAboutText(response.data.data)
+    }
+    loadUsers()
+
+  }, []);
 
   return (
-    <div>
-      <Row xs={1} md={2} className="g-4">
-        {data.length > 0 ?
-          data.map((activity) => {
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+
+      flexWrap: 'wrap',
+      gap: '1em',
+
+    }}>
+
+      {
+        aboutText.length > 0 ?
+          aboutText.map((activity) => {
             return (
-              <Card bg="light" border="dark " key={activity.id} style={{ width: '20rem' }} >
+              <Card className="gap-1" bg="light" key={activity.id} style={{ width: '20rem' }} >
                 <Card.Img variant="top" src={activity.image} />
                 <Card.Body>
                   <Card.Title>{activity.name} </Card.Title>
                   <Card.Text>
                     {activity.description}
                   </Card.Text>
-                  <Button variant="outline-dark" href={activity.linkedinUrl}>Linkdin</Button>
-                  <Button variant="outline-dark" href={activity.facebookUrl}>Facebook</Button>
+                  <div style={{ display: 'flex', justifyContent: "center", columnGap: "5%" }}>   <Button variant="outline-dark" href={activity.linkedinUrl}>Linkdin</Button>
+                    <Button variant="outline-dark" href={activity.facebookUrl}>Facebook</Button>
+                  </div>
                 </Card.Body>
               </Card>
             )
           })
           :
           <p>No Hay Miembros Cargados en la Base de Datos</p>
-        }
-      </Row>
-
-    </div>
+      }
+    </div >
   );
 }
-MiembrosAbout.defaultProps = {
-  data: [
-    {
-      "id": 258,
-      "name": "Osvaldo Olivera",
-      "image": "http://ongapi.alkemy.org/storage/kw2hNujhuq.jpeg",
-      "description": "Presidente",
-      "facebookUrl": "https://www.facebook.com/100075440371054/",
-      "linkedinUrl": "https://www.linkedin.com/in/osvaldo-olivera-785b78226/",
-      "created_at": "2021-11-09T20:22:05.000000Z",
-      "updated_at": "2021-11-23T04:34:29.000000Z",
-      "deleted_at": null,
-      "group_id": null
-    },
-    {
-      "id": 259,
-      "name": "Nahuel Narváez",
-      "image": "http://ongapi.alkemy.org/storage/z5x07SOhzl.jpeg",
-      "description": "Secretario",
-      "facebookUrl": "https://www.facebook.com/100075448260572/",
-      "linkedinUrl": "https://www.linkedin.com/in/nahuel-narváez-099b89226/",
-      "created_at": "2021-11-09T23:25:38.000000Z",
-      "updated_at": "2021-11-23T04:18:18.000000Z",
-      "deleted_at": null,
-      "group_id": null
-    },
-    {
-      "id": 261,
-      "name": "Griselda Germán",
-      "image": "http://ongapi.alkemy.org/storage/dR97wbZyUN.jpeg",
-      "description": "Tesorera",
-      "facebookUrl": "https://www.facebook.com/100074894726439/",
-      "linkedinUrl": "https://www.linkedin.com/in/griselda-germán-137b83226/",
-      "created_at": "2021-11-10T00:32:23.000000Z",
-      "updated_at": "2021-11-23T04:31:58.000000Z",
-      "deleted_at": null,
-      "group_id": null
-    }
-  ]
 
-}
 
 export default MiembrosAbout
 
