5ab3e115ad90aa02a7dd52f59dcc80caa7e9cca6
Ignacio Hardoy <hartoyy@hotmail.com>
2021-12-03 19:38:13 -0300

    prettier added

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,30 +1,31 @@
 import axios from 'axios';
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 01, //Aqui va el ID del equipo!!
+  },
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get('https://jsonplaceholder.typicode.com/users', config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
 export const postRequest = async (path_url, dataBody) => {
-    const response = await axios.post(
-      `http://ongapi.alkemy.org/${path_url}`,
-      JSON.stringify({
-        dataBody,
-      }),
-      {
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-          "Authorization": `Bearer ${token}`
-        },
-      }
-    );
-  };
+  const response = await axios.post(
+    `http://ongapi.alkemy.org/${path_url}`,
+    JSON.stringify({
+      dataBody,
+    }),
+    {
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+};
 
-export default Get 
+export default Get;
