e5e3f4ac64946bed32ad2661b796814c076917f9
JuanStromanIlz <juanstroman@gmail.com>
2021-12-03 10:23:00 -0300

    first commit, A new Contact method was created to make http calls for the contact sections

---
+++ src/Services/Contacts.js
@@ -0,0 +1,22 @@
+//In this file import the reusable methods for petitions and customize for contact use.
+// baseURL: http://ongapi.alkemy.org/api/contacts
+
+const Get = () => {};
+
+const Post = () => {};
+
+const GetById = (id) => {};
+
+const Put = (id) => {};
+
+const Delete = (id) => {};
+
+const Contact = {
+  Get,
+  Post,
+  GetById,
+  Put,
+  Delete,
+};
+
+export default Contact;
