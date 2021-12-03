a07060d43d33602c99c5cde5c4f314b96eca4617
Edgar Britez <edgarfbritez@gmail.com>
2021-12-03 18:10:10 -0300

    initial commit

---
+++ src/Components/Donations/Donation.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import styles from './Donation.module.css';
+
+function Donation() {
+  return <div>Donation Works!</div>;
+}
+
+export default Donation;
---
+++ src/Components/Donations/Thanks.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import styles from './Thanks.module.css';
+
+function Thanks() {
+  return <div>Thanks Works!</div>;
+}
+
+export default Thanks;
