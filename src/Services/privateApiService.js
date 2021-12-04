96a8542d6b88ff187c596b6e765d3b4d516835b5
Mauricio Romero <mauricionromero@hotmail.com>
2021-12-03 22:44:05 -0300

    OT104-72 added patchRequest function and test file for privateApiService.js

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,30 @@
-import axios from 'axios';
+import axios from "axios";
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 104, //Aqui va el ID del equipo!!
+  },
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get("https://jsonplaceholder.typicode.com/users", config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+// expected format:
+// url:"https://jsonplaceholder.typicode.com/users"
+// id: 7
+// body: {}
+export const patchRequest = async (url, id, body) => {
+  // const config = VerifyToken();
+  try {
+    const res = await axios.patch(url + id, body, config);
+    return res;
+  } catch (error) {
+    return error;
+  }
+};
+
+export default Get;
