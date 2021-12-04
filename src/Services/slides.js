a82ddf19fa8df7f4c10d3932097c830a39b1327d
Mauricio Romero <mauricionromero@hotmail.com>
2021-12-04 19:10:45 -0300

    OT104-81 refactored to create a custom hook

--- src/Services/slides.js
+++ src/Services/slides.js
@@ -1,24 +1,34 @@
 import { getRequest } from "./publicApiService";
 import { postRequest, putRequest, deleteRequest } from "./privateApiService";
 
-const baseUrl = "http://ongapi.alkemy.org/api/slides/";
+export const useSlides = () => {
+  const baseUrl = "http://ongapi.alkemy.org/api/slides/";
 
-export const getSlides = () => {
-  return getRequest(baseUrl);
-};
+  const getSlides = () => {
+    return getRequest(baseUrl);
+  };
 
-export const getSlide = (id) => {
-  return getRequest(baseUrl + id);
-};
+  const getSlide = (id) => {
+    return getRequest(baseUrl + id);
+  };
 
-export const createSlide = (slide) => {
-  return postRequest(baseUrl, slide);
-};
+  const createSlide = (slide) => {
+    return postRequest(baseUrl, slide);
+  };
 
-export const updateSlide = (slide) => {
-  return putRequest(baseUrl + slide.id, slide);
-};
+  const updateSlide = (slide) => {
+    return putRequest(baseUrl + slide.id, slide);
+  };
 
-export const deleteSlide = (id) => {
-  return deleteRequest(baseUrl + id);
+  const deleteSlide = (id) => {
+    return deleteRequest(baseUrl + id);
+  };
+
+  return {
+    getSlides,
+    getSlide,
+    createSlide,
+    updateSlide,
+    deleteSlide,
+  };
 };
