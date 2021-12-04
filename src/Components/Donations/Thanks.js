50c122d407578392c9675164a128eb5a51efeee2
Edgar Britez <edgarfbritez@gmail.com>
2021-12-03 22:04:14 -0300

    finish the Thanks component, create a image folder to add the LOGO

--- src/Components/Donations/Thanks.js
+++ src/Components/Donations/Thanks.js
@@ -1,8 +1,24 @@
 import React from "react";
-// import styles from './Thanks.module.css';
+import styles from "./Thanks.module.css";
+import logo from "../../images/LOGO-SOMOS-MAS.png";
+import { useHistory } from "react-router-dom";
 
 function Thanks() {
-  return <div>Thanks Works!</div>;
+  const history = useHistory();
+
+  return (
+    <div className={styles.thanksMessageContainer}>
+      <h2 className={styles.thanksMessage}>Muchas gracias por tu ayuda!</h2>
+      <div>
+        <img
+          src={logo}
+          alt=""
+          onClick={() => history.push("/")}
+          className={styles.thanksLogo}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Thanks;
