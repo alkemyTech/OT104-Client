23ec873398c55e4673015a349cf8dab37918b934
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-12-05 18:04:27 -0300

    comment line deletion

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -1,15 +1,9 @@
 import axios from "axios";
 
-// const config = {
-//   headers: {
-//     Group: 01, //Aqui va el ID del equipo!!
-//   },
-// };
-
-const Get = async (url, id) => {
+const get = async (url, id) => {
   const res =
     id === null ? await axios.get(url) : await axios.get(`${url}/${id}`);
   return res;
 };
 
-export default Get;
+export default get;
