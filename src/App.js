4f51fb1d339bbee27cd569a6d554609b21bf94a9
JuanStromanIlz <juanstroman@gmail.com>
2021-12-06 22:15:54 -0300

    remove fragment

--- src/App.js
+++ src/App.js
@@ -16,24 +16,22 @@ import ActivitiesList from './Components/Activities/ActivitiesList';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
-          <Route path='/create-activity' component={ActivitiesForm} />
-          <Route path='/create-category' component={CategoriesForm} />
-          <Route path='/create-news' component={NewsForm} />
-          <Route path='/backoffice/create-slide' component={SlidesForm} />
-          <Route path='/backoffice/activities' component={ActivitiesList} />
-          <Route path='/create-testimonials' component={TestimonialForm} />
-          <Route path='/create-user' component={UserForm} />
-          <Route path='/create-member' component={MembersForm} />
-          <Route path='/create-project' component={ProjectsForm} />
-          <Route path='/school-campaign' component={SchoolCampaign} />
-          <Route path='/toys-campaign' component={ToysCampaign} />
-        </Switch>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Switch>
+        {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+        <Route path='/create-activity' component={ActivitiesForm} />
+        <Route path='/create-category' component={CategoriesForm} />
+        <Route path='/create-news' component={NewsForm} />
+        <Route path='/backoffice/create-slide' component={SlidesForm} />
+        <Route path='/backoffice/activities' component={ActivitiesList} />
+        <Route path='/create-testimonials' component={TestimonialForm} />
+        <Route path='/create-user' component={UserForm} />
+        <Route path='/create-member' component={MembersForm} />
+        <Route path='/create-project' component={ProjectsForm} />
+        <Route path='/school-campaign' component={SchoolCampaign} />
+        <Route path='/toys-campaign' component={ToysCampaign} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
