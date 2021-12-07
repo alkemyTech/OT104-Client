51c6e31a7953b812de4fbaec2efb3858e58b0f0b
JuanStromanIlz <juanstroman@gmail.com>
2021-12-06 22:23:17 -0300

    main merged

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
