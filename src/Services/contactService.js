846cab76330bfe34b082ff5a4740127ef0f438ad
JuanStromanIlz <juanstroman@gmail.com>
2021-12-06 21:42:45 -0300

    now contactService have a object with all the methods

--- src/Services/contactService.js
+++ src/Services/contactService.js
@@ -3,22 +3,32 @@ import { postRequest, putRequest, deleteRequest } from './privateApiService';
 
 const baseURL = 'http://ongapi.alkemy.org/api/contacts';
 
-export const getContacts = () => {
+const getContacts = () => {
   return getRequest(baseURL);
 };
 
-export const createContact = (contact) => {
+const createContact = (contact) => {
   return postRequest(baseURL, contact);
 };
 
-export const getContact = (id) => {
+const getContact = (id) => {
   return getRequest(baseURL + id);
 };
 
-export const editContact = (contact) => {
+const editContact = (contact) => {
   return putRequest(baseURL + contact.id, contact);
 };
 
-export const deleteContact = (id) => {
+const deleteContact = (id) => {
   return deleteRequest(baseURL + id);
 };
+
+const contactService = {
+  get: getContacts,
+  getById: getContact,
+  create: createContact,
+  edit: editContact,
+  delete: deleteContact,
+};
+
+export default contactService;
