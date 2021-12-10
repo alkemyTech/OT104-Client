0a97f6a3263e35ba3b46f2658ca8e34c2cc6ea73
Ignacio Hardoy <hartoyy@hotmail.com>
2021-12-10 18:31:27 -0300

    reducer for activities created

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import activitiesSlice from '../features/categories/activitiesSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    activities: activitiesSlice,
   },
 });
---
+++ src/features/categories/activitiesSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getActivities = createAsyncThunk(
+  'activities/getActivities',
+  async (_, { rejectWithValue }) => {
+    try {
+      let res = await fetch('http://ongapi.alkemy.org/api/activities').then(
+        (data) => data.json()
+      );
+      console.log(res);
+      if (!res.success) {
+        return rejectWithValue(res.message);
+      }
+      return res;
+    } catch ({ message }) {
+      return rejectWithValue(message);
+    }
+  }
+);
+
+const initialState = {
+  data: [],
+  status: '',
+  message: '',
+};
+
+const activitiesSlice = createSlice({
+  name: 'activities',
+  initialState,
+  extraReducers(builder) {
+    builder
+      .addCase(getActivities.pending, (state) => {
+        state.status = 'pending';
+      })
+      .addCase(getActivities.fulfilled, (_, { payload }) => {
+        return {
+          data: payload.data,
+          status: 'fulfilled',
+          message: payload.message,
+        };
+      })
+      .addCase(getActivities.rejected, (_, { payload }) => {
+        return {
+          data: undefined,
+          status: 'rejected',
+          message: payload,
+        };
+      });
+  },
+});
+
+export default getActivities.reducer;
