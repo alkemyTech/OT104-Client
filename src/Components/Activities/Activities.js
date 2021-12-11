278044aa20f5deb9affef65f5a012203fe6cb839
Ignacio Hardoy <hartoyy@hotmail.com>
2021-12-11 16:57:19 -0300

    loeader added for activities and activities detail

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ActivitiesList from './ActivitiesList';
 import ActivitiesTitle from './ActivitiesTitle';
+import Spinner from '../Spinner/Spinner';
+
 const Activities = () => {
+  const [activitiesList, setActivitiesList] = useState([]);
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await axios.get(
+        `http://ongapi.alkemy.org/api/activities`
+      );
+      setActivitiesList(response.data.data);
+    };
+    loadUsers();
+  }, []);
+
   return (
     <Container fluid>
-      <Row style={{ marginTop: '5%' }}>
-        <Col></Col>
-        <Col xs={5}>
-          <ActivitiesTitle />
-        </Col>
-        <Col></Col>
-      </Row>
-      <Row style={{ marginTop: '2%' }}>
-        <Col></Col>
-        <Col xs={10}>
-          <div style={{ marginTop: '5%' }}>
-            <ActivitiesList data={undefined} />
-          </div>
-        </Col>
-        <Col></Col>
-      </Row>
+      {activitiesList.length > 0 ? (
+        <>
+          <Row style={{ marginTop: '5%' }}>
+            <Col></Col>
+            <Col xs={5}>
+              <ActivitiesTitle />
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row style={{ marginTop: '2%' }}>
+            <Col></Col>
+            <Col xs={10}>
+              <div style={{ marginTop: '5%' }}>
+                <ActivitiesList data={activitiesList} />
+              </div>
+            </Col>
+            <Col></Col>
+          </Row>
+        </>
+      ) : (
+        <Row style={{ marginTop: '5%' }}>
+          <Col xs={10}>
+            <Spinner />
+          </Col>
+        </Row>
+      )}
     </Container>
   );
 };
--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,21 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import '../CardListStyles.css';
 import Card from 'react-bootstrap/Card';
 
 const ActivitiesList = ({ data }) => {
-  const [activitiesList, setActivitiesList] = useState([]);
-
-  useEffect(() => {
-    const loadUsers = async () => {
-      const response = await axios.get(
-        `http://ongapi.alkemy.org/api/activities`
-      );
-      setActivitiesList(response.data.data);
-    };
-    loadUsers();
-  }, []);
-  console.log(activitiesList);
   return (
     <div
       style={{
@@ -25,27 +12,23 @@ const ActivitiesList = ({ data }) => {
         gap: '3rem',
       }}
     >
-      {activitiesList.length > 0 ? (
-        activitiesList.map((activity) => {
-          return (
-            <Card
-              className='gap-2'
-              bg='light'
-              key={activity.id.toString()}
-              style={{ width: '30rem' }}
-            >
-              <Card.Header>
-                <h5 style={{ color: 'Crimson' }}>{activity.name}</h5>
-              </Card.Header>
-              <Card.Body>
-                <Card.Text>{activity.description}</Card.Text>
-              </Card.Body>
-            </Card>
-          );
-        })
-      ) : (
-        <p>No hay actividades</p>
-      )}
+      {data.map((activity) => {
+        return (
+          <Card
+            className='gap-2'
+            bg='light'
+            key={activity.id.toString()}
+            style={{ width: '30rem' }}
+          >
+            <Card.Header>
+              <h5 style={{ color: 'Crimson' }}>{activity.name}</h5>
+            </Card.Header>
+            <Card.Body>
+              <Card.Text>{activity.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </div>
   );
 };
