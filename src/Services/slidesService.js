8c53dda883cf8d14425b4f4bdf20724a74af33f6
Mauricio Romero <mauricionromero@hotmail.com>
2021-12-11 11:52:55 -0300

    OT104-81 completed test and changed delete function to async

--- src/Services/slidesService.js
+++ src/Services/slidesService.js
@@ -1,26 +1,30 @@
-import { getRequest } from "./publicApiService";
-import { postRequest, putRequest, deleteRequest } from "./privateApiService";
+import {
+  postRequest,
+  putRequest,
+  deleteRequest,
+  getRequest,
+} from "./privateApiService";
 
-const baseUrl = "http://ongapi.alkemy.org/api/slides/";
+const baseUrl = "http://ongapi.alkemy.org/api/slides";
 
 const getSlides = () => {
   return getRequest(baseUrl);
 };
 
 const getSlide = (id) => {
-  return getRequest(baseUrl + id);
+  return getRequest(`${baseUrl}/${id}`);
 };
 
 const createSlide = (slide) => {
   return postRequest(baseUrl, slide);
 };
 
-const updateSlide = (slide) => {
-  return putRequest(baseUrl + slide.id, slide);
+const updateSlide = (slide, id) => {
+  return putRequest(baseUrl, id, slide);
 };
 
 const deleteSlide = (id) => {
-  return deleteRequest(baseUrl + id);
+  return deleteRequest(baseUrl, id);
 };
 
 export default {
