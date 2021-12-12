971b0d1ca46da8c7c4c94faa48b7cf08c8d94510
Edgar Britez <edgarfbritez@gmail.com>
2021-12-12 19:03:41 -0300

    I moved the useEffect to Home and Newness will get the data by props

--- src/Components/Home/Newness.jsx
+++ src/Components/Home/Newness.jsx
@@ -1,51 +1,30 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import { Container } from "react-bootstrap";
 import { Card } from "react-bootstrap";
-import { Spinner } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import "./styles/newness.css";
 
-function Newness() {
-  const [news, setNews] = useState([]);
-  useEffect(() => {
-    const getNewsData = async () => {
-      await axios
-        .get("http://ongapi.alkemy.org/api/news?limit=4")
-        .then((newData) => {
-          setNews((news) => newData.data.data);
-        })
-        .catch((err) => {
-          alert("Error", err);
-        });
-    };
-    getNewsData();
-  }, []);
-
+function Newness({ news }) {
   return (
     <Container id="news-container">
-      {news.length === 0 ? (
-        <Spinner animation="border" variant="primary" />
-      ) : (
-        <Container id="cards-container" className="p-1">
-          {news.map((data, index) => {
-            return (
-              <Card id="card" key={index} className="mb-2">
-                <Card.Img variant="top" src={data.image} />
-                <Card.Body>
-                  <Card.Title>{data.name}</Card.Title>
-                  <Button variant="primary" data-id={data.id}>
-                    Leer mas
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-          <Container className="text-center">
-            <Button>Ver mas</Button>
-          </Container>
+      <Container id="cards-container" className="p-1">
+        {news.map((data, index) => {
+          return (
+            <Card id="card" key={index} className="mb-2">
+              <Card.Img variant="top" src={data.image} />
+              <Card.Body>
+                <Card.Title>{data.name}</Card.Title>
+                <Button variant="primary" data-id={data.id}>
+                  Leer mas
+                </Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+        <Container className="text-center">
+          <Button>Ver mas</Button>
         </Container>
-      )}
+      </Container>
     </Container>
   );
 }
