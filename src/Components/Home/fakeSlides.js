9ad22462aa420c6b42000e346880c435fcd3ba85
Edgar Britez <edgarfbritez@gmail.com>
2021-12-12 18:35:28 -0300

    adding fake data for testing

---
+++ src/Components/Home/fakeSlides.js
@@ -0,0 +1,22 @@
+const fakeSlides = [
+  {
+    name: "Slide 1",
+    description: "Description 1",
+    image:
+      "https://images.unsplash.com/photo-1537655780520-1e392ead81f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Slide 2",
+    description: "Description 2",
+    image:
+      "https://images.unsplash.com/photo-1519340241574-2cec6aef0c01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",
+  },
+  {
+    name: "Slide 3",
+    description: "Description 3",
+    image:
+      "https://images.unsplash.com/photo-1512253080918-79cf0c2e0650?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  },
+];
+
+export default fakeSlides;
