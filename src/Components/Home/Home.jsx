359472dde4d0f635d0f03b83bc3cf2d1ecd49192
Edgar Britez <edgarfbritez@gmail.com>
2021-12-12 18:47:58 -0300

    move Slider to Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import Newness from "./Newness";
-import Slider from "./Slider";
+import Slider from "./Slider/Slider";
+import service from "../../Services/slidesService";
+import Loader from "../../Components/Home/Loader";
+import fakeSlides from "./fakeSlides";
+
 function Home() {
+  // we need to replace the fakeSlides with the real data from the server
+  // remove the fakeSlides from the code
+
+  const [slides, setSlides] = React.useState([...fakeSlides]);
+  // uncomment the lines below to get slides from server
+  // const getSlides = async () => {
+  //   const res = await service.getAll();
+  //   const slidesFromServer = res.data.data;
+  //   setSlides([...slidesFromServer]);
+  // };
+  // React.useEffect(() => {
+  //   getSlides();
+  // }, []);
+
   return (
     <div className="home-container">
-      <Slider />
+      <Slider slides={slides} />
       <h1 className="text-center m-2">
         Bienvenidos <br />a <br />
         Somos más
       </h1>
+      {slides.length === 0 && <Loader />}
       <h2 className="text-center mt-3">Ultimas Novedades</h2>
       <Newness />
     </div>
