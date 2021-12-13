900a32339e9618fe6083a5173fbb8b705483e40d
Edgar Britez <edgarfbritez@gmail.com>
2021-12-12 22:52:07 -0300

    I moved the useEffect to About and remove unnecesary imports

--- src/Components/About/TextoAbout.jsx
+++ src/Components/About/TextoAbout.jsx
@@ -1,24 +1,13 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios";
+import React from "react";
 // import { useDispatch } from 'react-redux'
 
-const TextoAbout = () => {
+const TextoAbout = ({ aboutDescription }) => {
   // const dispatch = useDispatch()
-  const [aboutText, setAboutText] = useState('');
-
-  useEffect(() => {
-    const loadUsers = async () => {
-      const response = await axios.get(`http://ongapi.alkemy.org/api/organization`)
-      setAboutText(response.data.data.short_description)
-    }
-    loadUsers()
-  }, []);
-
   return (
     <div>
-      <h5 >{aboutText}</h5>
+      <h5>{aboutDescription}</h5>
     </div>
-  )
-}
+  );
+};
 
-export default TextoAbout
+export default TextoAbout;
