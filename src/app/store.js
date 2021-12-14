7ba905830985bea3216bcc592ae3b17cb9be95bb
Ignacio Hardoy <hartoyy@hotmail.com>
2021-12-14 18:29:33 -0300

    delete files from another ticket

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import activitiesSlice from '../features/categories/activitiesSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
-    activities: activitiesSlice,
   },
 });
