d1b738318959276e6643b4c0e6fc10810825d4e7
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-12-14 19:16:19 -0300

    add try&catch

--- src/Services/homeApiService.js
+++ src/Services/homeApiService.js
@@ -2,23 +2,39 @@ import axios from "axios";
 const baseUrl = "http://ongapi.alkemy.org/api/";
 
 const getMethod = async () => {
-  const response = await axios.get(`${baseUrl}`);
-  return response.data;
+  try {
+    const response = await axios.get(`${baseUrl}`);
+    return response.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 const postMethod = async (path, bodyData) => {
-  const response = await axios.post(`${baseUrl}/${path}`, bodyData);
-  return response.data;
+  try {
+    const response = await axios.post(`${baseUrl}/${path}`, bodyData);
+    return response.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 const putMethod = async (path, id, bodyData) => {
-  const response = await axios.put(`${baseUrl}/${path}/${id}`, bodyData);
-  return response.data;
+  try {
+    const response = await axios.put(`${baseUrl}/${path}/${id}`, bodyData);
+    return response.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 const deleteMethod = async (path, id) => {
-  const response = await axios.delete(`${baseUrl}/${path}/${id}`);
-  return response.data;
+  try {
+    const response = await axios.delete(`${baseUrl}/${path}/${id}`);
+    return response.data;
+  } catch (err) {
+    return err;
+  }
 };
 
 export const homeServices = {
