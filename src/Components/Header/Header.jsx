ab2c8b3aad6e6b4e52e2e9791779258961269362
Alejo Agustín Martinez Aseijas <martinezalejo123a@gmail.com>
2021-12-17 14:34:13 -0300

    add styles

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Navbar, Container, Nav, Image } from "react-bootstrap";
-import { NavLink, useHistory } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const navBarItems = [
   { name: "Inicio", path: "/", id: 1 },
   { name: "Nosotros", path: "/Nosotros", id: 2 },
   { name: "Contacto", path: "/contacto", id: 3 },
-  { name: "Campañas", path: "/campañas", id: 4 },
 ];
 import somosMasLogo from "../../images/LOGO-SOMOS-MAS.png";
+import "./style.scss";
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" className="navbar-container">
       <Container>
         <Navbar.Brand>
-          <Image src={somosMasLogo} alt="somos-mas-logo" />
+          <Image src={somosMasLogo} alt="somos-mas-logo" className="logo-img" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -24,8 +24,10 @@ function Header() {
             ) : (
               navBarItems.map((item) => {
                 return (
-                  <Nav.Link key={item.id} active="bg primary">
-                    <NavLink to={item.path}>{item.name}</NavLink>
+                  <Nav.Link key={item.id}>
+                    <NavLink to={item.path} className="link-to-section">
+                      {item.name}
+                    </NavLink>
                   </Nav.Link>
                 );
               })
