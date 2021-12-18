5bef0db24df83f9d80b6d3065019105422efd908
Edgar Britez <edgarfbritez@gmail.com>
2021-12-18 07:23:02 -0300

    moved logic to About parent

--- src/Components/About/TituloAbout.jsx
+++ src/Components/About/TituloAbout.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect } from "react";
-import { fetchOrgData } from "../../features/about/aboutReducer";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 
-const TituloAbout = () => {
-  const dispatch = useDispatch();
-  const { orgData } = useSelector((state) => state.about);
-
-  useEffect(() => {
-    dispatch(fetchOrgData());
-  }, [dispatch]);
-
+const TituloAbout = ({ ongName }) => {
   return (
     <div>
-      <h1>{orgData.name}</h1>
+      <h1>{ongName}</h1>
     </div>
   );
 };
