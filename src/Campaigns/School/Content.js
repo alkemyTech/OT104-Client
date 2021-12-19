3f4f7e8bab2b11fa2afba8b4b97fc77b990ef97f
Edgar Britez <edgarfbritez@gmail.com>
2021-12-19 17:16:15 -0300

    add CountDown  to Content

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import CountDown from "./CountDown";
 
 const Content = () => {
   return (
     <div>
-      
+      <CountDown />
     </div>
   );
-}
- 
+};
+
 export default Content;
