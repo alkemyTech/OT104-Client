fa8e0273a96bef883d803673c8ac1eea263caff3
Edgar Britez <edgarfbritez@gmail.com>
2021-12-19 18:29:40 -0300

    add background to desktop devices

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -5,16 +5,18 @@ import logoCampain from "./images/logotipo_campania_materiales_escolares.png";
 
 const Content = () => {
   return (
-    <div className={styles.container}>
-      <h2 className={styles.title}>Sumate a nuestra campaña</h2>
-      <div className={styles.logoBox}>
-        <img src={logoCampain} alt="" />
+    <div className={styles.bg}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Sumate a nuestra campaña</h2>
+        <div className={styles.logoBox}>
+          <img src={logoCampain} alt="" />
+        </div>
+        <div className={styles.about}>
+          <h3 className="text-center">Inicia el 10 de Enero</h3>
+          <h4 className="text-center">Buenos Aires, 12hs</h4>
+        </div>
+        <CountDown />
       </div>
-      <div className={styles.about}>
-        <h3 className="text-center">Inicia el 10 de Enero</h3>
-        <h4 className="text-center">Buenos Aires, 12hs</h4>
-      </div>
-      <CountDown />
     </div>
   );
 };
