98347b12f127423853b21c84cca3c16ed5dc93ef
Mauricio Romero <mauricionromero@hotmail.com>
2021-12-23 17:46:17 -0300

    OT104-151 created test file

--- src/Components/News/NewsForm.js
+++ src/Components/News/NewsForm.js
@@ -72,7 +72,7 @@ const NewsForm = ({ newToEdit = false }) => {
     let isError = false;
 
     const newsSubmission = axios.create({
-      baseURL: process.env.REACT_APP_URL_ONG,
+      baseURL: "http://ongapi.alkemy.org/api",
     });
 
     if (newToEdit) {
