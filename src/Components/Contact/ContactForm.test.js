56f713f330eb1a5a9228039ca184e037a5441086
JuanStromanIlz <juanstroman@gmail.com>
2021-12-29 10:23:11 -0300

    merge and fix errors

--- src/Components/Contact/ContactForm.test.js
+++ src/Components/Contact/ContactForm.test.js
@@ -1,7 +1,7 @@
 import { screen, render, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
-import { store } from "../../app";
+import store from "../../app/store";
 import ContactForm from "./ContactForm";
 import contactService from "../../Services/contactService";
 
@@ -12,9 +12,9 @@ const form = {
   message: "some lorem",
 };
 
-const App = ({ children }) => <Provider store={store}>{children}</Provider>;
-
 const contact = jest.spyOn(contactService, "create");
+
+const App = ({ children }) => <Provider store={store}>{children}</Provider>;
 
 describe("contact-form", () => {
   it("trigger submit button without fill the inputs trigger a error message", async () => {
@@ -40,68 +40,80 @@ describe("contact-form", () => {
     });
   });
 
-  // it("submit form and get error from petition", async () => {
-  //   contact.mockReturnValue(new Error());
-  //   render(<ContactForm />);
-  //   const nameInput = screen.getByLabelText(/Nombre/i);
-  //   const emailInput = screen.getByLabelText(/E-mail/i);
-  //   const phoneInput = screen.getByLabelText(/Teléfono/i);
-  //   const messageInput = screen.getByLabelText(/Mensaje/i);
+  it("submit form and get error from petition", async () => {
+    contact.mockReturnValue(new Error());
+    render(
+      <App>
+        <ContactForm />
+      </App>
+    );
+    const nameInput = screen.getByLabelText(/Nombre/i);
+    const emailInput = screen.getByLabelText(/E-mail/i);
+    const phoneInput = screen.getByLabelText(/Teléfono/i);
+    const messageInput = screen.getByLabelText(/Mensaje/i);
 
-  //   userEvent.type(nameInput, form.name);
-  //   userEvent.type(emailInput, form.email);
-  //   userEvent.type(phoneInput, form.phone);
-  //   userEvent.type(messageInput, form.message);
-  //   userEvent.click(screen.getByRole("button", { name: "Enviar Mensaje" }));
+    userEvent.type(nameInput, form.name);
+    userEvent.type(emailInput, form.email);
+    userEvent.type(phoneInput, form.phone);
+    userEvent.type(messageInput, form.message);
+    userEvent.click(screen.getByRole("button", { name: "Enviar Mensaje" }));
 
-  //   await waitFor(() => {
-  //     expect(
-  //       screen.getByText("Ocurrio un error al intentar realizar la petición")
-  //     ).toBeInTheDocument();
-  //   });
-  // });
+    await waitFor(() => {
+      expect(
+        screen.getByText("Ocurrio un error al intentar realizar la petición")
+      ).toBeInTheDocument();
+    });
+  });
 
-  // it("submit form and get error from api", async () => {
-  //   contact.mockReturnValue({
-  //     data: { success: false },
-  //   });
-  //   render(<ContactForm />);
-  //   const nameInput = screen.getByLabelText(/Nombre/i);
-  //   const emailInput = screen.getByLabelText(/E-mail/i);
-  //   const phoneInput = screen.getByLabelText(/Teléfono/i);
-  //   const messageInput = screen.getByLabelText(/Mensaje/i);
+  it("submit form and get error from api", async () => {
+    contact.mockReturnValue({
+      data: { success: false },
+    });
+    render(
+      <App>
+        <ContactForm />
+      </App>
+    );
+    const nameInput = screen.getByLabelText(/Nombre/i);
+    const emailInput = screen.getByLabelText(/E-mail/i);
+    const phoneInput = screen.getByLabelText(/Teléfono/i);
+    const messageInput = screen.getByLabelText(/Mensaje/i);
 
-  //   userEvent.type(nameInput, form.name);
-  //   userEvent.type(emailInput, form.email);
-  //   userEvent.type(phoneInput, form.phone);
-  //   userEvent.type(messageInput, form.message);
-  //   userEvent.click(screen.getByRole("button", { name: "Enviar Mensaje" }));
+    userEvent.type(nameInput, form.name);
+    userEvent.type(emailInput, form.email);
+    userEvent.type(phoneInput, form.phone);
+    userEvent.type(messageInput, form.message);
+    userEvent.click(screen.getByRole("button", { name: "Enviar Mensaje" }));
 
-  //   await waitFor(() => {
-  //     expect(
-  //       screen.getByText("Ocurrio un error al intentar guardar este contacto")
-  //     ).toBeInTheDocument();
-  //   });
-  // });
+    await waitFor(() => {
+      expect(
+        screen.getByText("Ocurrio un error al intentar guardar este contacto")
+      ).toBeInTheDocument();
+    });
+  });
 
-  // it("success to save a new contact", async () => {
-  //   contact.mockReturnValue({ data: { success: true } });
-  //   render(<ContactForm />);
-  //   const nameInput = screen.getByLabelText(/Nombre/i);
-  //   const emailInput = screen.getByLabelText(/E-mail/i);
-  //   const phoneInput = screen.getByLabelText(/Teléfono/i);
-  //   const messageInput = screen.getByLabelText(/Mensaje/i);
+  it("success to save a new contact", async () => {
+    contact.mockReturnValue({ data: { success: true } });
+    render(
+      <App>
+        <ContactForm />
+      </App>
+    );
+    const nameInput = screen.getByLabelText(/Nombre/i);
+    const emailInput = screen.getByLabelText(/E-mail/i);
+    const phoneInput = screen.getByLabelText(/Teléfono/i);
+    const messageInput = screen.getByLabelText(/Mensaje/i);
 
-  //   userEvent.type(nameInput, form.name);
-  //   userEvent.type(emailInput, form.email);
-  //   userEvent.type(phoneInput, form.phone);
-  //   userEvent.type(messageInput, form.message);
-  //   userEvent.click(screen.getByRole("button", { name: "Enviar Mensaje" }));
+    userEvent.type(nameInput, form.name);
+    userEvent.type(emailInput, form.email);
+    userEvent.type(phoneInput, form.phone);
+    userEvent.type(messageInput, form.message);
+    userEvent.click(screen.getByRole("button", { name: "Enviar Mensaje" }));
 
-  //   await waitFor(() => {
-  //     expect(
-  //       screen.getByText("Contacto guardado con exito")
-  //     ).toBeInTheDocument();
-  //   });
-  // });
+    await waitFor(() => {
+      expect(
+        screen.getByText("Contacto guardado con exito")
+      ).toBeInTheDocument();
+    });
+  });
 });
