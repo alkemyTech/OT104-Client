d4f7c61fdf29b9652ef74a9209b5e87f351ffeca
silvioaa <silvio.acevedo@gmail.com>
2022-01-05 13:20:54 -0300

    Text and content overflow problems fixed.

--- src/Components/About/TextoAbout.jsx
+++ src/Components/About/TextoAbout.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 const TextoAbout = ({ aboutDescription }) => {
   return (
     <Container>
-      <h5 className="text-center">{aboutDescription}</h5>
+      <h5 dangerouslySetInnerHTML={{ __html: aboutDescription }} className="text-center"></h5>
     </Container>
   );
 };
