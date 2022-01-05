20922038921b35ad170fc21f90be732b2402f31b
Amadeo de la Peña <amadeodlp@hotmail.com>
2022-01-05 16:17:05 -0300

    fixed contact component

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -86,69 +86,71 @@ const ContactForm = () => {
   }
 
   return (
-    <Container style={{ maxWidth: "30rem" }} className="card bg-light p-3 my-4">
+    <>
       {<h3 className="text mt-4 text-center">Contacto</h3>}
-      {position && (
-        <MapContainer
-          center={JSON.parse(position)}
-          zoom={13}
-          scrollWheelZoom={false}
-          style={{ height: "450px", width: "50%", margin: "auto" }}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={JSON.parse(position)}></Marker>
-        </MapContainer>
-      )}
-      <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validate={handleErrors}
-      >
-        {() => (
-          <Form className="p-3">
-            <div>
-              <FormLabel className="mt-3" htmlFor="name">
-                Nombre:
-              </FormLabel>
-              <Field id="name" name="name" type="text" as={FormControl} />
-              <ErrorMessage name="name" variant="danger" component={Alert} />
-            </div>
-            <div>
-              <FormLabel className="mt-3" htmlFor="email">
-                E-mail:
-              </FormLabel>
-              <Field id="email" name="email" type="text" as={FormControl} />
-              <ErrorMessage name="email" variant="danger" component={Alert} />
-            </div>
-            <div>
-              <FormLabel className="mt-3" htmlFor="phone">
-                Teléfono:
-              </FormLabel>
-              <Field id="phone" name="phone" type="number" as={FormControl} />
-              <ErrorMessage name="phone" variant="danger" component={Alert} />
-            </div>
-            <div>
-              <FormLabel className="mt-4" htmlFor="message">
-                Mensaje:
-              </FormLabel>
-              <Field id="message" name="message" as={FormControl} />
-              <ErrorMessage name="message" variant="danger" component={Alert} />
-            </div>
-            <Button
-              className="mt-4 mb-3"
-              style={{ width: "100%" }}
-              variant="primary"
-              type="submit"
-            >
-              Enviar Mensaje
-            </Button>
-          </Form>
+        {position && (
+          <MapContainer
+            center={JSON.parse(position)}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: "450px", width: "50%", margin: "auto" }}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={JSON.parse(position)}></Marker>
+          </MapContainer>
         )}
-      </Formik>
-    </Container>
+      <Container style={{ maxWidth: "30rem" }} className="card bg-light p-3 my-4">
+        <Formik
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          validate={handleErrors}
+        >
+          {() => (
+            <Form className="p-3">
+              <div>
+                <FormLabel className="mt-3" htmlFor="name">
+                  Nombre:
+                </FormLabel>
+                <Field id="name" name="name" type="text" as={FormControl} />
+                <ErrorMessage name="name" variant="danger" component={Alert} />
+              </div>
+              <div>
+                <FormLabel className="mt-3" htmlFor="email">
+                  E-mail:
+                </FormLabel>
+                <Field id="email" name="email" type="text" as={FormControl} />
+                <ErrorMessage name="email" variant="danger" component={Alert} />
+              </div>
+              <div>
+                <FormLabel className="mt-3" htmlFor="phone">
+                  Teléfono:
+                </FormLabel>
+                <Field id="phone" name="phone" type="number" as={FormControl} />
+                <ErrorMessage name="phone" variant="danger" component={Alert} />
+              </div>
+              <div>
+                <FormLabel className="mt-4" htmlFor="message">
+                  Mensaje:
+                </FormLabel>
+                <Field id="message" name="message" as={FormControl} />
+                <ErrorMessage name="message" variant="danger" component={Alert} />
+              </div>
+              <Button
+                className="mt-4 mb-3"
+                style={{ width: "100%" }}
+                variant="primary"
+                type="submit"
+              >
+                Enviar Mensaje
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </Container>
+    </>
   );
 };
 
